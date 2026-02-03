51535ec1c9fd3696373d6cda4c3c9f6adc4ef938
gauravkaushik-boop <gaurav.kaushik@rupyy.com>
2026-02-03 11:21:29 +0530

    testing ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { delay } from "@/lib/utils";
 import { BlogPostsResponse } from "@/models/BlogPost";
 import Link from "next/link";
 
+
+
 export default async function BlogPage() {
   const response = await fetch("https://dummyjson.com/posts");
   const { posts }: BlogPostsResponse = await response.json();
